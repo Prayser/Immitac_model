e6eaa2913193e20c41ae2ed1910708b34477aa94
Prayser <69678056+Prayser@users.noreply.github.com>
2022-03-11 16:54:26 +0700

    Add files via upload

---
+++ Elemenatry_Cellular_Automaton/Elemenatry_Cellular_Automaton/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace Elemenatry_Cellular_Automaton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inpRule = new System.Windows.Forms.NumericUpDown();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.inpRule)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 257);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rule";
+            // 
+            // inpRule
+            // 
+            this.inpRule.Location = new System.Drawing.Point(134, 257);
+            this.inpRule.Maximum = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            this.inpRule.Name = "inpRule";
+            this.inpRule.Size = new System.Drawing.Size(120, 22);
+            this.inpRule.TabIndex = 1;
+            this.inpRule.Value = new decimal(new int[] {
+            110,
+            0,
+            0,
+            0});
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(84, 313);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(152, 89);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start / Stop";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(347, 78);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(526, 426);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1017, 602);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.inpRule);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.inpRule)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown inpRule;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Elemenatry_Cellular_Automaton/Elemenatry_Cellular_Automaton/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Elemenatry_Cellular_Automaton
+{
+    public partial class Form1 : Form
+    {
+        const int maxGens = 10, maxCells = 10;
+
+        Dictionary<string, bool> Rules = new Dictionary<string, bool>();
+        bool[,] CellularMatrix;
+
+        Bitmap bitmap;
+        Graphics graph;
+        int lastGeneration;
+        bool action = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitCellGrid();
+        }
+        private void CreateFirstGeneration()
+        {
+            CellularMatrix = new bool[maxGens, maxCells];
+            lastGeneration = 0;
+            CellularMatrix[0, 1] = true;
+            CellularMatrix[0, 3] = true;
+            CellularMatrix[0, 5] = true;
+            CellularMatrix[0, 7] = true;
+            CellularMatrix[0, 9] = true;
+        }
+
+        private void InitCellGrid()
+        {
+            bitmap = new Bitmap(panel1.Width, panel1.Height);
+            graph = Graphics.FromImage(bitmap);
+
+            DrawLeftColumn();
+            DrawGridTemplate();
+        }
+
+        private void DrawCells()
+        {
+            SolidBrush solidBrush = new SolidBrush(Color.Black);
+
+            for (int genNum = 0; genNum < maxGens; genNum++)
+            {
+                for (int cellNum = 0; cellNum < maxCells; cellNum++)
+                {
+                    if (CellularMatrix[genNum, cellNum])
+                    {
+                        graph.FillRectangle(solidBrush, 30 + cellNum * 30, genNum * 30, 30, 30);
+                    }
+                }
+            }
+        }
+
+        private void DrawLeftColumn()
+        {
+            graph.FillRectangle(new SolidBrush(Color.White), new Rectangle(0, 0, 30, 300));
+
+            Font font = new Font("Calibri", 14);
+            SolidBrush solidBrush = new SolidBrush(Color.Black);
+
+            for (int i = 0; i < maxGens; i++)
+            {
+                graph.DrawString(i.ToString(), font, solidBrush, 6, i * 30 + 4);
+            }
+        }
+
+        private void DrawGridTemplate()
+        {
+            Pen pen = new Pen(Color.Black);
+
+            for (int i = 0; i < maxGens + 1; i++)
+            {
+                graph.DrawLine(pen, 0, i * 30, 330, i * 30);
+            }
+
+            for (int i = 0; i < maxCells + 2; i++)
+            {
+                graph.DrawLine(pen, i * 30, 0, i * 30, 300);
+            }
+        }
+
+        private string IntToFullBinString(int input, int length)
+        {
+            string l = Convert.ToString(input, 2);
+            while (l.Length < length) l = '0' + l;
+            return l;
+        }
+
+        private void SetRules()
+        {
+            string rule = IntToFullBinString((int)inpRule.Value, 8);
+
+            for (int i = 0; i < 8; i++)
+            {
+                Rules[IntToFullBinString(i, 3)] = Convert.ToBoolean(Convert.ToInt32(rule.Substring(7 - i, 1)));
+            }
+        }
+
+        private void CreateNextGeneration()
+        {
+            int prevGen = lastGeneration;
+            lastGeneration++;
+            for (int i = 0; i < maxCells; i++)
+            {
+                string rule = (CellularMatrix[prevGen, mod(i - 1, maxCells - 1)] ? "1" : "0")
+                    + (CellularMatrix[prevGen, i] ? "1" : "0")
+                    + (CellularMatrix[prevGen, mod(i + 1, maxCells - 1)] ? "1" : "0");
+
+                CellularMatrix[lastGeneration, i] = Rules[rule];
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+            if (action)
+            {
+                timer1.Stop();
+            }
+            else
+            {
+                CreateFirstGeneration();
+                graph.Clear(Color.White);
+                SetRules();
+                timer1.Start();
+            }
+            action = !action;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (lastGeneration >= maxGens - 1)
+            {
+                timer1.Stop();
+                action = !action;
+                return;
+            }
+
+            CreateNextGeneration();
+            DrawLeftColumn();
+            DrawGridTemplate();
+            DrawCells();
+            panel1.Invalidate();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(bitmap, Point.Empty);
+        }
+
+        private int mod(int x, int m)
+        {
+            return (x % m + m) % m;
+        }
+    }
+}
